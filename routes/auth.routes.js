cdbfb14ec59c3fb2d38553489980f22d60fbf7f6
vijay18399 <vijayreddy18399@gmail.com>
2022-06-29 16:13:05 +0530

    added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -13,5 +13,5 @@ router.post("/signin", authController.signin);
 router.post("/refreshtoken", authController.refreshToken);
 router.post("/update", authController.update);
 // get users count
-router.get("/user-count", apiController.getUserCount);
+router.get("/user-count", authController.getUserCount);
 module.exports = router;
